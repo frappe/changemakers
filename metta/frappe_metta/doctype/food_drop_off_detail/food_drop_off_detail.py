addccd14ced2ab346e7da49e973dfee8023dbfc1
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-10-20 22:29:30 +0530

    feat: Food Distribution Record

---
+++ metta/frappe_metta/doctype/food_drop_off_detail/food_drop_off_detail.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, hussain@frappe.io and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class FoodDropOffDetail(Document):
+	pass
