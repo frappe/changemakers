d61fbdc74ec1275a8a5c508f5cb71c3deda92a78
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-10-24 23:17:30 +0530

    feat: Validate if shelter exit for a given admission already exists

--- metta/frappe_metta/doctype/shelter_home_exit_record/shelter_home_exit_record.py
+++ metta/frappe_metta/doctype/shelter_home_exit_record/shelter_home_exit_record.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2022, hussain@frappe.io and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
+
 class ShelterHomeExitRecord(Document):
-	pass
+    def before_insert(self):
+        self.validate_duplicate_exit()
+
+    def validate_duplicate_exit(self):
+        """throws if an exit for the admission already exists"""
+        exit_exists = frappe.db.exists("Shelter Home Exit Record", {
+            "admission_record": self.admission_record})
+
+        if exit_exists:
+            frappe.throw(
+                "An exit has already been created for the given admission")
