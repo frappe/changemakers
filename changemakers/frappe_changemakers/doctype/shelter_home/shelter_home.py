56c345e6d3926ce0af64f6ecceaff41da7ce1aa5
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-26 20:16:29 +0530

    fix: pincode field in shelter home

--- changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
+++ changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
@@ -1,8 +1,15 @@
 # Copyright (c) 2022, hussain@frappe.io and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
 class ShelterHome(Document):
-	pass
+	def validate(self):
+		self.validate_pincode()
+	
+	def validate_pincode(self):
+		if len(str(self.pincode)) != 6:
+			frappe.throw("Pincode should be a numeric value with exactly 6 digits")
+	
