f3a4c3c3548b0306c01ecb11edca8a1d2bc55c3d
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-27 13:05:27 +0530

    feat: Health Camp Record

---
+++ changemakers/frappe_changemakers/doctype/health_camp_record/health_camp_record.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2022, hussain@frappe.io and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HealthCampRecord(Document):
+    def validate(self):
+        self.set_total_patients_screened()
+
+    def set_total_patients_screened(self):
+        self.total_patients_screened = (
+            (self.number_of_males or 0)
+            + (self.number_of_females or 0)
+            + (self.number_of_children or 0)
+            + (self.number_of_others or 0)
+        )
