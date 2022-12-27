01059a2409cec2983513d5a94ae9f7eefccba4b9
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-27 18:53:50 +0530

    fix: establishment year validation

--- changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
+++ changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
@@ -7,15 +7,18 @@ from frappe.model.document import Document
 
 
 class ShelterHome(Document):
-	def validate(self):
-		self.validate_pin_code()
-		self.validate_establishment_year()
+    def validate(self):
+        self.validate_pin_code()
+        self.validate_establishment_year()
 
-	def validate_pin_code(self):
-		if len(str(self.pin_code)) != 6:
-			frappe.throw("Pin Code should be a numeric value with exactly 6 digits")
+    def validate_pin_code(self):
+        if len(str(self.pin_code)) != 6:
+            frappe.throw("Pin Code should be a numeric value with exactly 6 digits")
 
-	def validate_establishment_year(self):
-		if len(str(self.establishment_year)) != 4:
-			frappe.throw("Invalid Value for Establishment Year")
-
+    def validate_establishment_year(self):
+        if (
+            self.establishment_year
+            and self.establishment_year > 0
+            and len(str(self.establishment_year)) != 4
+        ):
+            frappe.throw("Invalid Value for Establishment Year")
