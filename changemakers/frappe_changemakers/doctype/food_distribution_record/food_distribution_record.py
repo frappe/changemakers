a6b5aacf6bee5a2b5a1fb6852130a5d586bdf6a6
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-12-29 08:28:34 +0530

    fix: misc

--- changemakers/frappe_changemakers/doctype/food_distribution_record/food_distribution_record.py
+++ changemakers/frappe_changemakers/doctype/food_distribution_record/food_distribution_record.py
@@ -1,17 +1,32 @@
 # Copyright (c) 2022, hussain@frappe.io and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
+
 class FoodDistributionRecord(Document):
-	def before_save(self):
-		self.set_distributed_packets()
+    def before_save(self):
+        self.set_distributed_packets()
 
-	def set_distributed_packets(self):
-		total_distributed_packets = 0
+    def on_submit(self):
+        self.set_returned_packets()
 
-		for entry in self.drop_offs:
-			total_distributed_packets += entry.number_of_packets
-		
-		self.packets_distributed = total_distributed_packets
+    def set_returned_packets(self):
+        self.packets_returned = (
+            self.packets_taken_for_distribution - self.packets_distributed
+        )
+
+    def set_distributed_packets(self):
+        total_distributed_packets = 0
+
+        for entry in self.drop_offs:
+            total_distributed_packets += entry.number_of_packets
+
+        self.packets_distributed = total_distributed_packets
+
+        if total_distributed_packets > self.packets_taken_for_distribution:
+            frappe.throw(
+                "Sum of distributed packets cannot be more than packets taken for distribution"
+            )
