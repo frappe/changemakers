087f5f448ee9b231871dff18c7a17f9e37f4b6a9
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-01-03 18:11:03 +0530

    feat: frappe icons

---
+++ frontend/src/components/icons/index.ts
@@ -0,0 +1,7 @@
+import BeneficiaryIcon from "@/components/icons/Beneficiary.vue"
+import RescueIcon from "@/components/icons/Rescue.vue"
+
+export const FrappeIcons = {
+	BeneficiaryIcon,
+	RescueIcon,
+}
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,34 +1,37 @@
 <template>
-  <ion-page>
-    <ion-content :fullscreen="true">
-      <div
-        class="flex flex-col self-center items-center justify-center h-full space-y-3"
-      >
-        <h2 class="text-gray-700 font-medium text-7xl">{{ beneficiaryDoc.doc && beneficiaryDoc.doc.gender }}</h2>
-        <Badge color="green">Completed</Badge>
-      </div>
-    </ion-content>
-  </ion-page>
+	<ion-page>
+		<ion-content :fullscreen="true">
+			<div
+				class="flex flex-col self-center items-center justify-center h-full space-y-3"
+			>
+				<h2 class="text-gray-700 font-medium text-7xl">
+					{{ beneficiaryDoc.doc && beneficiaryDoc.doc.gender }}
+				</h2>
+				<Badge color="green">Completed</Badge>
+				<div>
+					<FrappeIcons.BeneficiaryIcon />
+					<FrappeIcons.RescueIcon />
+				</div>
+			</div>
+		</ion-content>
+	</ion-page>
 </template>
 
 <script lang="ts" setup>
-import {
-  IonPage,
-  IonContent,
-} from "@ionic/vue";
+import { IonPage, IonContent } from "@ionic/vue"
+import { FrappeIcons } from "@/components/icons"
 
-import { createDocumentResource } from "frappe-ui";
-
-import { Beneficiary } from "@/../types/FrappeChangemakers/Beneficiary";
-import { DocumentResource } from "@/typing/resource";
+import { createDocumentResource } from "frappe-ui"
+import { Beneficiary } from "@/../types/FrappeChangemakers/Beneficiary"
+import { DocumentResource } from "@/typing/resource"
 
 const beneficiaryDoc: DocumentResource<Beneficiary> = createDocumentResource({
-  doctype: "Beneficiary",
-  name: "BENE-2022-12-00002",
-  onSuccess(d: Beneficiary) {
-    console.log(d.gender);
-  },
-});
+	doctype: "Beneficiary",
+	name: "BENE-2022-12-00002",
+	onSuccess(d: Beneficiary) {
+		console.log(d.gender)
+	},
+})
 
-beneficiaryDoc.reload();
+beneficiaryDoc.reload()
 </script>
