eb44f979f3a77d91007bd90d9b5d25529a605db0
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-01-09 10:04:12 +0530

    fix: code format

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue}",
-    './node_modules/frappe-ui/src/components/**/*.{vue,js,ts}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  presets: [
-    require('frappe-ui/src/utils/tailwind.config')
-  ]
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx,vue}",
+		"./node_modules/frappe-ui/src/components/**/*.{vue,js,ts}",
+		"./frappe-ui/src/components/**/*.{vue,js,ts}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	presets: [require("frappe-ui/src/utils/tailwind.config")],
 }
