464088138e2c6d8f91b25efde4593fc359d01967
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-01-09 09:50:17 +0530

    chore: prod build setup

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -35,7 +35,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory("/c/"),
 	routes,
 })
 
