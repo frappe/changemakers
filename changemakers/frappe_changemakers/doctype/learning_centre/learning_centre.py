b955cfb24f9eb3ad52b99d035b622d610cf5ceb8
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-01-10 16:20:48 +0530

    fix: data type and validation for pin_code

--- changemakers/frappe_changemakers/doctype/learning_centre/learning_centre.py
+++ changemakers/frappe_changemakers/doctype/learning_centre/learning_centre.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2022, hussain@frappe.io and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
+
 class LearningCentre(Document):
-	pass
+	def validate(self):
+		self.validate_pin_code()
+
+	def validate_pin_code(self):
+		if not self.pin_code:
+			return
+
+		if not self.pin_code.isnumeric():
+			frappe.throw("Pin Code should contain only numeric values")
+
+		if len(str(self.pin_code)) != 6:
+			frappe.throw("Pin Code should be a numeric value with exactly 6 digits")
--- changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
+++ changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
@@ -7,18 +7,24 @@ from frappe.model.document import Document
 
 
 class ShelterHome(Document):
-    def validate(self):
-        self.validate_pin_code()
-        self.validate_establishment_year()
+	def validate(self):
+		self.validate_pin_code()
+		self.validate_establishment_year()
 
-    def validate_pin_code(self):
-        if len(str(self.pin_code)) != 6:
-            frappe.throw("Pin Code should be a numeric value with exactly 6 digits")
+	def validate_pin_code(self):
+		if not self.pin_code:
+			return
 
-    def validate_establishment_year(self):
-        if (
-            self.establishment_year
-            and self.establishment_year > 0
-            and len(str(self.establishment_year)) != 4
-        ):
-            frappe.throw("Invalid Value for Establishment Year")
+		if not self.pin_code.isnumeric():
+			frappe.throw("Pin Code should contain only numeric values")
+
+		if len(str(self.pin_code)) != 6:
+			frappe.throw("Pin Code should be a numeric value with exactly 6 digits")
+
+	def validate_establishment_year(self):
+		if (
+			self.establishment_year
+			and self.establishment_year > 0
+			and len(str(self.establishment_year)) != 4
+		):
+			frappe.throw("Invalid Value for Establishment Year")
