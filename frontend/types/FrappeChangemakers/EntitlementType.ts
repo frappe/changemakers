d6b59fe02f6036d254e51d7d212ba6cc41ac5ac3
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-02-03 19:41:37 +0530

    chore: more permission fixes

---
+++ frontend/types/FrappeChangemakers/EntitlementType.ts
@@ -0,0 +1,15 @@
+
+export interface EntitlementType{
+	creation: string
+	name: string
+	modified: string
+	owner: string
+	modified_by: string
+	docstatus: 0 | 1 | 2
+	parent?: string
+	parentfield?: string
+	parenttype?: string
+	idx?: number
+	/**	Description : Small Text	*/
+	description?: string
+}
