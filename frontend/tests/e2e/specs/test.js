6303792f4aa96a82773b42679332608140a63dac
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-02-13 12:33:52 +0530

    chore: remove unused code

--- frontend/tests/e2e/specs/test.js
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe("My First Test", () => {
-	it("Visits the app root url", () => {
-		cy.visit("/")
-		cy.contains("ion-content", "Tab 1 page")
-	})
-})
--- frontend/tests/unit/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Dashboard from '@/views/Dashboard.vue'
-
-describe('Tab1Page.vue', () => {
-  it('renders tab 1 Tab1Page', () => {
-    // const wrapper = mount(Dashboard)
-  })
-})
