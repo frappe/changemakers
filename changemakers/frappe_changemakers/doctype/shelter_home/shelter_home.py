91264c0e7dcc51c0dc81c09980183cf6906374b2
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-02-17 20:06:31 +0530

    fix: vacant beds not more than total capacity of shelter

--- changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
+++ changemakers/frappe_changemakers/doctype/shelter_home/shelter_home.py
@@ -9,6 +9,14 @@ class ShelterHome(Document):
 	def validate(self):
 		self.validate_pin_code()
 		self.validate_establishment_year()
+		self.validate_vacant_beds_not_exceed_capacity()
+
+	def validate_vacant_beds_not_exceed_capacity(self):
+		total_capacity = self.total_capacity or 0
+		vacant_beds = self.num_vacant_available or 0
+
+		if vacant_beds > total_capacity:
+			frappe.throw("Number of Vacant Beds cannot be more than the Total Capacity!")
 
 	def validate_pin_code(self):
 		if not self.pin_code:
--- changemakers/frappe_changemakers/doctype/shelter_home/test_shelter_home.py
+++ changemakers/frappe_changemakers/doctype/shelter_home/test_shelter_home.py
@@ -1,9 +1,50 @@
 # Copyright (c) 2022, hussain@frappe.io and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestShelterHome(FrappeTestCase):
-	pass
+	@classmethod
+	def setUp(cls):
+		test_s = frappe.get_doc(doctype="State", name="shelter_test_state").insert()
+		test_d = frappe.get_doc(
+			doctype="District", name="shelter_test_district", state=test_s.name
+		).insert()
+		test_z = frappe.get_doc(
+			doctype="Zone", name="shelter_test_zone", district=test_d.name
+		).insert()
+		frappe.get_doc(doctype="Ward", name="shelter_test_ward", zone=test_z.name).insert()
+
+	def test_vacant_beds_less_than_capacity(self):
+		# create test records
+		test_shelter_home = frappe.get_doc(
+			{
+				"doctype": "Shelter Home",
+				"name": "shelter_test",
+				"ownership_of_property": "Own",
+				"state": "shelter_test_state",
+				"district": "shelter_test_district",
+				"zone": "shelter_test_zone",
+				"ward": "shelter_test_ward",
+			}
+		).insert()
+
+		# works for valid cases
+		test_shelter_home.total_capacity = 100
+		test_shelter_home.num_vacant_available = 90
+		test_shelter_home.save()
+
+		with self.assertRaises(frappe.ValidationError):
+			test_shelter_home.total_capacity = 100
+			test_shelter_home.num_vacant_available = 120
+			test_shelter_home.save()
+
+	@classmethod
+	def tearDown(cls):
+		frappe.delete_doc("shelter_test", force=True)
+		frappe.delete_doc("shelter_test_ward", force=True)
+		frappe.delete_doc("shelter_test_zone", force=True)
+		frappe.delete_doc("shelter_test_district", force=True)
+		frappe.delete_doc("shelter_test_state", force=True)
