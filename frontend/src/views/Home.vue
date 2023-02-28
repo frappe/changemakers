9e579e98d755f8e19b5207d10222d90b8235d788
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-02-28 11:56:05 +0530

    fix: better name for tabs

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,12 +3,12 @@
 		<ion-tabs>
 			<ion-router-outlet></ion-router-outlet>
 			<ion-tab-bar slot="bottom">
-				<ion-tab-button tab="tab1" href="/tabs/dashboard">
+				<ion-tab-button tab="dashboardTab" href="/tabs/dashboard">
 					<ion-icon :icon="gridOutline"></ion-icon>
 					<ion-label>{{ t("navigation.dashboard") }}</ion-label>
 				</ion-tab-button>
 
-				<ion-tab-button tab="tab2" href="/tabs/account">
+				<ion-tab-button tab="AccountTab" href="/tabs/account">
 					<ion-icon :icon="personOutline"></ion-icon>
 					<ion-label>{{ t("navigation.account") }}</ion-label>
 				</ion-tab-button>
