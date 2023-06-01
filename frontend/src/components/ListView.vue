2e402f65c30ea43101be78acc16df9cb0f8996f5
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-06-01 13:43:13 +0530

    feat: better ui

--- frontend/src/components/ListView.vue
+++ frontend/src/components/ListView.vue
@@ -1,6 +1,7 @@
 <template>
 	<ion-page>
 		<ion-content :fullscreen="true">
+			<!-- Header -->
 			<div
 				class="flex w-full flex-row items-center justify-between bg-white p-4"
 			>
@@ -13,90 +14,9 @@
 					</Button>
 					<h2 class="p-0 text-2xl font-semibold text-gray-900">Rescues</h2>
 				</div>
-			</div>
-
-			<span v-if="documents.data">
-				{{ documents.data.length }} Record Fetched
-			</span>
-
-			<span>{{ getOptionForField("condition") }}</span>
-
-			<div
-				class="p-2"
-				v-for="fieldname in Object.keys(props.filterConfig)"
-				:key="fieldname"
-			>
-				<span>{{ fieldname.toUpperCase() }}</span>
-
-				<ion-list>
-					<ion-item>
-						<ion-select
-							:aria-label="fieldname"
-							interface="action-sheet"
-							placeholder="Select filter type"
-							:value="
-								filterFieldTypeOptionMap[
-									props.filterConfig[fieldname]['type']
-								][0]
-							"
-							v-model="untransformedFilters[fieldname].type"
-						>
-							<ion-select-option
-								v-for="(option, index) in filterFieldTypeOptionMap[
-									props.filterConfig[fieldname]['type']
-								]"
-								:value="option"
-								:key="option"
-								:selected="index === 0"
-								>{{ option }}</ion-select-option
-							>
-						</ion-select>
-					</ion-item>
-				</ion-list>
-
-				<!-- render input field based on the selected type of filter -->
-				<div>
-					<Input
-						type="number"
-						placeholder="Value"
-						v-model="untransformedFilters[fieldname].value"
-						v-if="
-							props.filterConfig[fieldname]['type'] === 'number' &&
-							untransformedFilters[fieldname].type
-						"
-					/>
-
-					<FormField
-						type="link"
-						:placeholder="`Select ${props.filterConfig[fieldname]['doctype']}`"
-						v-model="untransformedFilters[fieldname].value"
-						:doctype="props.filterConfig[fieldname]['doctype']"
-						v-if="
-							props.filterConfig[fieldname]['type'] === 'link' &&
-							untransformedFilters[fieldname].type
-						"
-					/>
-
-					<FormField
-						type="select"
-						:options="getOptionForField(fieldname)"
-						:placeholder="`Select ${fieldname}`"
-						v-model="untransformedFilters[fieldname].value"
-						v-if="
-							props.filterConfig[fieldname]['type'] === 'select' &&
-							untransformedFilters[fieldname].type
-						"
-					/>
-				</div>
-			</div>
-
-			<Button appearance="primary" @click="applyFilters">Apply Filters</Button>
-			<Button appearance="minimal" @click="clearFilters">Clear Filters</Button>
-			<!-- <pre>{{ titleFieldName }}</pre> -->
-
-			<!-- <RouterLink
-					v-if="showRescues"
-					:to="{ name: 'NewRescueForm' }"
+				<RouterLink
+					v-if="documents.data"
+					:to="{ name: `New${doctype}Form` }"
 					v-slot="{ navigate }"
 				>
 					<Button @click="navigate" appearance="primary" icon-left="plus"
@@ -105,13 +25,14 @@
 				</RouterLink>
 			</div>
 
-			<div class="m-4 space-y-3" v-if="showRescues">
-				<div class="flex flex-row items-center space-x-1">
+			<!-- If data is visible -->
+			<div v-if="documents.data" class="flex flex-col p-5">
+				<div class="mb-5 flex flex-row items-center space-x-2">
 					<Input
 						type="text"
-						placeholder="Search by beneficiary name"
+						:placeholder="`Search ${doctype}s`"
 						icon-left="search"
-						class="w-full rounded-full bg-white p-1 focus:bg-white"
+						class="w-full rounded-full bg-white p-[5px] focus:bg-white"
 						@input="(v) => (searchQuery = v)"
 						:modelValue="searchQuery"
 					/>
@@ -119,107 +40,55 @@
 					<Button
 						appearance="white"
 						icon="filter"
-						class="h-full rounded-full p-4"
-						@click="filters.showFilterDialog = !filters.showFilterDialog"
+						class="h-full w-fit rounded-2xl px-3 py-3"
+						@click="toggleShowFilters"
 					></Button>
 				</div>
-				<div
-					v-if="filters.showFilterDialog"
-					class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-				>
-					<div
-						class="flex w-full max-w-[90%] flex-col gap-3 rounded-lg bg-white px-5 py-4 text-sm font-semibold text-gray-700"
+
+				<div v-for="(data, index) in documents.data" :key="data.name">
+					<router-link
+						custom
+						:to="{ name: `${doctype}Form`, params: { id: data.name } }"
+						v-slot="{ href, navigate }"
 					>
-						<div class="mb-2 flex justify-between">
-							<h3 class="text-xl font-semibold">Filter Rescues</h3>
-							<Button
-								@click="filters.showFilterDialog = !filters.showFilterDialog"
-								icon-left="x"
-								appearance="minimal"
-								class="w-0"
-							></Button>
-						</div>
-						<div class="flex flex-col gap-1">
-							<Input
-								label="Gender"
-								v-model="filters.gender"
-								type="select"
-								:options="genderOptions"
-							/>
-						</div>
-						<div class="flex flex-col gap-1">
-							<span>State</span>
-							<Autocomplete
-								:options="stateOptions"
-								v-model="filters.state"
-								placeholder="Select State"
-							/>
-						</div>
-						<HotspotFilter @setFilter="handleSetFilter" />
-
-						<div class="mt-6 flex justify-start gap-3">
-							<Button @click="resetFilters" appearance="danger">Reset</Button>
-							<Button appearance="primary" @click="applyFilters"
-								>Apply Filters</Button
-							>
-						</div>
-					</div>
-				</div>
-				<div>
-					<ul>
-						<li
-							v-for="rescue in filteredRescue"
-							class="border-b-[1px] bg-white first-of-type:first:rounded-t-xl last-of-type:last:rounded-b-xl"
-							:key="rescue.name"
+						<a
+							class="flex w-full flex-row items-center justify-between gap-[14px] border-b-[1px] bg-white px-4 py-3"
+							:class="{
+								'rounded-t-xl': index === 0,
+								'rounded-b-xl': index === documents.data.length - 1,
+							}"
+							:href="href"
+							@click="navigate"
 						>
-							<router-link
-								custom
-								:to="{ name: 'RescueForm', params: { id: rescue.name } }"
-								v-slot="{ href, navigate }"
-							>
-								<a
-									class="flex flex-row items-center justify-between px-3 py-[14px]"
-									:href="href"
-									@click="navigate"
-								>
-									<div
-										class="flex flex-row items-center justify-start gap-[14px]"
-									>
-										<FrappeIcons.RescueIcon class="text-gray-500" />
-										<div class="flex flex-col">
-											<h3 class="text-lg font-semibold text-gray-700">
-												{{ rescue.first_name }} {{ rescue.last_name }},
-												{{ rescue.age }}, {{ rescue.gender }}
-											</h3>
-											<time
-												class="text-base text-gray-600"
-												:datetime="rescue.rescued_at"
-												>{{
-													dayjs(rescue.rescued_at).format("HH:mmA, DD-MM-YYYY")
-												}}</time
-											>
-										</div>
-									</div>
-
-									<div>
-										<FeatherIcon
-											class="h-[18px] w-[18px]"
-											name="chevron-right"
-										/>
-									</div>
-								</a>
-							</router-link>
-						</li>
-					</ul>
+							<div class="flex flex-row items-center justify-start gap-[14px]">
+								<FrappeIcons.RescueIcon class="text-gray-700" />
+								<div class="flex flex-col">
+									<h1 class="text-lg font-semibold text-gray-700">
+										{{ data.beneficiary }}
+									</h1>
+									<h4 class="text-base text-gray-600">
+										{{ data.name }} | {{ data.rescue_by }}
+									</h4>
+								</div>
+							</div>
+							<div>
+								<FeatherIcon
+									class="h-[18px] w-[18px] text-gray-700"
+									name="chevron-right"
+								/>
+							</div>
+						</a>
+					</router-link>
 				</div>
 			</div>
+			<!-- If there is no data -->
 			<div
 				v-else
 				class="flex h-full flex-col items-center justify-center gap-y-1.5"
 			>
 				<h3 class="font-medium">No rescue data yet</h3>
 				<RouterLink
-					:to="{ name: 'NewRescueForm' }"
+					:to="{ name: `New${doctype}Form` }"
 					v-slot="{ navigate }"
 					class="flex w-full justify-center"
 				>
@@ -231,19 +100,114 @@
 						>Add Rescue Data</Button
 					>
 				</RouterLink>
-			</div> -->
+			</div>
+
+			<!-- FILTERS -->
+			<div
+				v-if="showFilters"
+				class="fixed inset-0 z-50 flex items-end justify-center bg-black bg-opacity-50"
+			>
+				<div class="flex h-fit w-full flex-col rounded-t-xl bg-white p-5">
+					<div class="flex justify-between">
+						<h2 class="font-semibold text-gray-800">Filters</h2>
+						<Button @click="toggleShowFilters" icon="x" appearance="minimal">
+						</Button>
+					</div>
+					<div
+						class="px-2 py-1"
+						v-for="fieldname in Object.keys(props.filterConfig)"
+						:key="fieldname"
+					>
+						<span class="text-lg font-medium text-gray-800">{{
+							fieldname.charAt(0).toUpperCase() +
+							fieldname.slice(1).toLowerCase()
+						}}</span>
+
+						<div class="flex items-center gap-4">
+							<ion-list class="h-[28px] w-1/3 rounded-lg p-0">
+								<ion-item class="w-full rounded-md">
+									<ion-select
+										class="-top-4 m-0 appearance-none rounded-md text-base"
+										:aria-label="fieldname"
+										interface="action-sheet"
+										placeholder="Select"
+										:value="
+											filterFieldTypeOptionMap[
+												props.filterConfig[fieldname]['type']
+											][0]
+										"
+										v-model="untransformedFilters[fieldname].type"
+									>
+										<ion-select-option
+											class="m-0 p-0 text-base"
+											v-for="(option, index) in filterFieldTypeOptionMap[
+												props.filterConfig[fieldname]['type']
+											]"
+											:value="option"
+											:key="option"
+											:selected="index === 0"
+											>{{ option }}</ion-select-option
+										>
+									</ion-select>
+								</ion-item>
+							</ion-list>
+
+							<!-- render input field based on the selected type of filter -->
+							<div class="h-fit w-full">
+								<Input
+									type="number"
+									placeholder="Value"
+									v-model="untransformedFilters[fieldname].value"
+									v-if="
+										props.filterConfig[fieldname]['type'] === 'number' &&
+										untransformedFilters[fieldname].type
+									"
+								/>
+
+								<FormField
+									type="link"
+									:placeholder="`Select ${props.filterConfig[fieldname]['doctype']}`"
+									v-model="untransformedFilters[fieldname].value"
+									:doctype="props.filterConfig[fieldname]['doctype']"
+									v-if="
+										props.filterConfig[fieldname]['type'] === 'link' &&
+										untransformedFilters[fieldname].type
+									"
+								/>
+
+								<FormField
+									type="select"
+									:options="getOptionForField(fieldname)"
+									:placeholder="`Select ${fieldname}`"
+									v-model="untransformedFilters[fieldname].value"
+									v-if="
+										props.filterConfig[fieldname]['type'] === 'select' &&
+										untransformedFilters[fieldname].type
+									"
+								/>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-2 py-3">
+						<Button appearance="primary" @click="applyFilters"
+							>Apply Filters</Button
+						>
+						<Button @click="clearFilters">Clear Filters</Button>
+					</div>
+				</div>
+			</div>
 		</ion-content>
 	</ion-page>
 </template>
 
 <script lang="ts" setup>
 import { ref, computed, reactive } from "vue"
-import { createResource } from "frappe-ui"
-import { IonPage, IonContent } from "@ionic/vue"
+import { createResource, FeatherIcon } from "frappe-ui"
+import { IonPage, IonContent, IonModal } from "@ionic/vue"
 import { useRouter } from "vue-router"
 import { IonItem, IonList, IonSelect, IonSelectOption } from "@ionic/vue"
 import FormField from "./core/FormField.vue"
-import { setPlatformHelpers } from "ionicons/dist/types/stencil-public-runtime"
+import { FrappeIcons } from "./icons"
 
 const props = defineProps({
 	doctype: {
@@ -263,6 +227,12 @@ const props = defineProps({
 const router = useRouter()
 const documentMeta = ref(null)
 const untransformedFilters = reactive({})
+const searchQuery = ref("")
+const showFilters = ref(false)
+
+function toggleShowFilters() {
+	showFilters.value = !showFilters.value
+}
 
 function initializeFilters() {
 	if (props.filterConfig) {
--- frontend/src/views/rescue/List.vue
+++ frontend/src/views/rescue/List.vue
@@ -1,16 +1,14 @@
 <template>
-	<div>
-		<ListView
-			doctype="Rescue"
-			:filterConfig="{
-				gender: { type: 'link', doctype: 'Gender' },
-				state: { type: 'link', doctype: 'State' },
-				age: { type: 'number' },
-				condition: { type: 'select' },
-			}"
-			:sort="{ field: 'rescued_at', direction: 'asc' }"
-		/>
-	</div>
+	<ListView
+		doctype="Rescue"
+		:filterConfig="{
+			gender: { type: 'link', doctype: 'Gender' },
+			state: { type: 'link', doctype: 'State' },
+			age: { type: 'number' },
+			condition: { type: 'select' },
+		}"
+		:sort="{ field: 'rescued_at', direction: 'asc' }"
+	/>
 </template>
 
 <script setup lang="ts">
