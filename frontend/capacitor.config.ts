964945d3e6aaec797ffba7d2e8e7ccdba460df54
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-02 10:55:05 +0530

    fix: no server required

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -5,10 +5,6 @@ const config: CapacitorConfig = {
 	appName: "Changemakers",
 	webDir: "../changemakers/public/frontend",
 	bundledWebRuntime: false,
-	server: {
-		url: "http://172.31.8.142:8080/",
-		cleartext: true,
-	},
 	plugins: {
 		CapacitorHttp: {
 			enabled: true,
