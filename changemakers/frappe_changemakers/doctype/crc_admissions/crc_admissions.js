6561c7a910b9ab581e9b7bf6281242c7767170ad
maniamartial <martialmania19@gmail.com>
2023-10-20 10:36:21 +0300

    Adding CRC Admission form

--- changemakers/frappe_changemakers/doctype/crc_admissions/crc_admissions.js
+++ changemakers/frappe_changemakers/doctype/crc_admissions/crc_admissions.js
@@ -1,8 +1,7 @@
 // Copyright (c) 2023, hussain@frappe.io and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('CRC Admissions', {
+frappe.ui.form.on("CRC Admissions", {
 	// refresh: function(frm) {
-
 	// }
 });
--- changemakers/frappe_changemakers/doctype/crc_admissions/crc_admissions.py
+++ changemakers/frappe_changemakers/doctype/crc_admissions/crc_admissions.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2023, hussain@frappe.io and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
+from datetime import datetime
+from datetime import datetime
+
+
 class CRCAdmissions(Document):
-	pass
+    def validate(self):
+        self.calculate_actual_duration()
+
+    def calculate_actual_duration(self):
+        if self.date_of_admission:
+            date_of_admission = datetime.strptime(
+                self.date_of_admission, "%Y-%m-%d")
+            current_date = datetime.now()
+            duration = current_date - date_of_admission
+            actual_duration = duration.days
+            self.actual_duration = f"{int(actual_duration)} days"
